8ba3329ee9b37f463558f33273de8af5010a4a82
Track404 <111713000+Track404@users.noreply.github.com>
2024-03-11 16:30:12 +0100

    finish the gameboard and switching player turn

--- script.js
+++ script.js
@@ -10,27 +10,64 @@ function gameboard() {
     }
     
   }
-  board[0].splice(1,1,"X");
-  console.log(board);
+  console.log(`${players[0].name} to play`);
+  let columnChoice = prompt("choose your column 1-2-3");
+  let rowChoice = prompt("choose your row 1-2-3");
 
+  board[columnChoice][rowChoice]= players[0].marker
+  console.table(board);
+  
+  
+  outsideBoard= board;
 }
 
-function Player() {
-  let sign = 0;
-  let name = prompt("what your name ?")
-  let answer = prompt("Choose your sign between 1.X and 2.O");
-
-  if (answer === "X") {
-    sign = 1;
-  } else if (answer === "O") {
-    sign = 2;
-  } else {
+function game() {
+  gameboard();
+  let gamescore = false;
+  while (gamescore==false) {
+    if (
+      (outsideBoard[0][0] && outsideBoard[0][1] && outsideBoard[0][2]) ||
+      (outsideBoard[1][0] && outsideBoard[1][1] && outsideBoard[1][2]) ||
+      (outsideBoard[2][0] && outsideBoard[2][1] && outsideBoard[2][2]) ||
+      (outsideBoard[0][0] && outsideBoard[1][0] && outsideBoard[2][0]) ||
+      (outsideBoard[0][1] && outsideBoard[1][1] && outsideBoard[2][1]) ||
+      (outsideBoard[0][2] && outsideBoard[1][2] && outsideBoard[2][2]) ||
+      (outsideBoard[0][0] && outsideBoard[1][1] && outsideBoard[2][2]) ||
+      (outsideBoard[0][2] && outsideBoard[1][1] && outsideBoard[2][0])
+    ) {
+      gamescore = true;
+      console.log(`${player[0]} win the game`);
+    } else {
+      gameboard();
+      swapElements(players,0,1);
     
-    return "ERROR try again";
-    
-  }
-
-  console.log(`welcome ${name} you will play as ${sign}`);
-  return { name, sign };
+    }
 }
-gameboard();
+}
+  
+function gameturn() {
+ 
+
+}
+ 
+/*my player*/
+
+  let playerOne = {
+    name: "tim",
+    marker: "X",
+    number: "1"
+  };
+  
+  let playerTwo = {
+    name: "joe",
+    marker: "O",
+    number: "2"
+  };
+ const swapElements = (myArray, index1, index2) => {
+    [myArray[index1], myArray[index2]] = [myArray[index2], myArray[index1]];
+};
+  let players = [playerOne,playerTwo];
+  let outsideBoard = gameboard();
+  
+
+game();
